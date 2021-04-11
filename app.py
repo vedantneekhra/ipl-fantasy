74e2bb7548677e5666df7ceee641e1571e5d128e
Vedant Neekhra <vneekhra@ce.iitr.ac.in>
2021-04-12 01:26:15 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Basic home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
