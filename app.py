a80c5ed93cf25d24d4eb8b8e9ab45d6378fdafad
vedantneekhra <vneekhra@ce.iitr.ac.in>
2021-04-25 09:57:29 +0530

    added dynamic match list

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, redirect, render_template, request, url_for
-from flask_table import Table, Col
 import pandas as pd
 import datetime
 
 app = Flask(__name__)
 df = pd.read_excel('ipl_schedule.xlsx')
 NO_ROW = 7
-
-class Matchs(Table):
-    Team = Col('Team')
-    Date = Col('Date')
-    Time = Col('Time')
-    Venue = Col('Venue')
 
 ser = []
 for id, row in df.iterrows():
@@ -26,16 +19,13 @@ while df.index[0] < datetime.datetime.now():
 
 @app.route('/')
 def index():
-    """Basic home page.""" 
+    
     while df.index[0] < datetime.datetime.now():
         df.drop(df.index[0], inplace=True)
-    # tem = 
-    # print(tem.to_di)
-    # table = Matchs(tem)
-    # print(table.__html__())
-    print(type(df.head(7).to_html()))
+    
     tem = df.head(NO_ROW)
     tem.index = range(1, len(tem.index)+1)
+    
     return render_template('index.html', table = tem.to_html(classes='table table-sm', border=0, justify='left'))
 
 
